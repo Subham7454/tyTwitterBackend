31f3fae108f97bcd7ff2d314f8751dc14d69cb2f
Subham <shubhamrawat1843@gmail.com>
2024-10-01 17:34:26 +0530

    add error,response,aync handler

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
 import 'dotenv/config';
-
+import app from "app.js"
 import connectDB from "./db/indexDb.js";
 
 dotenv.config({
@@ -8,3 +8,12 @@ dotenv.config({
 })
 
 connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`server is runnig on port ${process.env.PORT}`)
+        })
+
+    })
+    .catch((err) => {
+        console.log("DB Connection failed !!!", err)
+    })
---
+++ src/utils/asycnHandler.js
@@ -0,0 +1,29 @@
+
+const asyncHandler = (requestHnadler) => {
+
+    (req, res, next) => {
+        Promise.resolve().catch((err) => next(err))
+    }
+
+}
+
+
+
+export default asyncHandler
+
+
+//const asyncHandler=()=>{}
+//const asyncHandler=(function)=>{()=>{}}
+//const asyncHandler=(function)=>async()=>{}
+
+/*const asyncHandler = (fn) => async (req, res, next) => {
+    try {
+  await fn(req,res,next)
+    } catch (error) {
+        res.status(err.code || 500).json({
+            success: false,
+            message: err.message
+        })
+    }
+
+}*/
