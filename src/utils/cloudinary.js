7a7638e2d417a0c1edd4ea405c0a876ef99343c0
Subham <shubhamrawat1843@gmail.com>
2024-10-01 23:33:41 +0530

    setup cloudinary and multer

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,26 @@
+import { v2 as cloudianry } from "cloudinary";
+import fs from "fs"
+
+cloudianry.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+
+const uploadOnCloudinary = async (loaclFilePath) => {
+    try {
+        if (!loaclFilePath) return null
+        //upload on cloudinary
+        const reponse = await cloudianry.uploader.upload(loaclFilePath, {
+            resource_type: "auto"
+        })
+        //file upload successfully
+        console.log("file uploaded successfully", reponse.url);
+        return reponse
+    } catch (error) {
+        fs.unlinkSync(loaclFilePath) //remove the locally save ulpoad file as the opration failed
+        return null;
+    }
+}
+export { uploadOnCloudinary }
